6299460057f189c55b3e20d2c70a4e8046fde6e0
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2021-02-26 20:15:55 +0700

    Download and Upload Storage Firebase

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -18,7 +18,7 @@ public class ResourceController : MonoBehaviour
         {
             // Menyimpan value yang di set ke _level pada Progress Data
             UserDataManager.Progress.ResourcesLevels[_index] = value;
-            UserDataManager.Save ();
+            UserDataManager.Save (true);
         }
 
         get
@@ -117,7 +117,7 @@ public class ResourceController : MonoBehaviour
             if (!UserDataManager.HasResources (_index))
             {
                 UserDataManager.Progress.ResourcesLevels.Add (_level);
-                UserDataManager.Save ();
+                UserDataManager.Save (true);
             }
         }
 
