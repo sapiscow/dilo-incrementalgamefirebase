bb8a04a050951a5ef9743c85b298ab19c1114165
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2021-02-28 18:26:41 +0700

    Add Analytics

--- Assets/Scripts/UserDataManager.cs
+++ Assets/Scripts/UserDataManager.cs
@@ -72,6 +72,8 @@ public static class UserDataManager
 
         if (uploadToCloud)
         {
+            AnalyticsManager.SetUserProperties ("gold", Progress.Gold.ToString ());
+
             byte[] data = Encoding.Default.GetBytes (json);
             StorageReference targetStorage = GetTargetCloudStorage ();
 
